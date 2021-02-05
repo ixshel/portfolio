eb84d4beac77b400c99f52270746deff93a73031
Salvador Solis <ch1.yomero.ss@gmail.com>
2021-02-05 16:47:55 -0600

    Correccion del link del cv

--- src/app/services/download-pdf.service.ts
+++ src/app/services/download-pdf.service.ts
@@ -9,7 +9,7 @@ export class DownloadPdfService {
 
   downloadPdf() {
     const DevPdfURL:string = "../../assets/documents/curriculum.pdf";
-    const ProdPdfURL:string = "../../assets/documents/curriculum.pdf";
+    const ProdPdfURL:string = "./assets/documents/curriculum.pdf";
     const pdfName:string = "curriculum";
     FileSaver.saveAs(ProdPdfURL, pdfName);
   }
