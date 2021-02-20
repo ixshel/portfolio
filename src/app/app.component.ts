bdf737369038de84d18c1d5c0a966048dc5cb4f3
Ixshel Escamilla <iescamilla@esri.com>
2021-02-19 22:54:29 -0800

    adding analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
+declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'proyecto';
 
-  ngOnInit(){
-  }
+ constructor(public router: Router) {
+  this.router.events.subscribe((event: any) => {
+    if (event instanceof NavigationEnd) {
+      gtag("config", "G-BGM4W18NSK", { page_path: event.urlAfterRedirects });
+    }
+  });
+ }
 }
